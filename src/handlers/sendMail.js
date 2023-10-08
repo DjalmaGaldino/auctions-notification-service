02816b202b931134fddef2c01f7e8af16e57094b
Djalma Galdino <galdino.dj@gmail.com>
2023-10-08 12:14:09 -0300

    adding sqs

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,8 +1,39 @@
+import AWS from 'aws-sdk'
 
+const ses = new AWS.SES({ region: 'eu-west-1'})
 
 async function sendMail(event, context) {
-  console.log(event)
-  return event
+  const record = event.Records[0]
+  console.log('record processing', record)
+
+  const email = JSON.parse(record.body)
+  const { subject, body, recipient } = email
+
+  const params = {
+    Source: 'albuquerquedjalma@gmail.com',
+    Destination: { 
+     ToAddresses: [ recipient ]
+    }, 
+    Message: {
+     Body: {    
+      Text: {
+       Charset: "UTF-8", 
+       Data: body
+      }
+     }, 
+     Subject: {
+      Charset: "UTF-8", 
+      Data: subject
+     }
+    }
+   }
+
+   try {
+    const result = await ses.sendEMail(params).promise()
+    console.log(result)
+   } catch (error) {
+    console.error(error)
+   }
 }
 
 export const handler = sendMail;
